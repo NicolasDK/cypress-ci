a46fbb53cd00ca2cfe2cf0e9d8e552f453258a2e
nicolas-ferreira <nicolas_tf@outlook.com>
2022-09-30 10:53:43 -0300

    add log to execution

--- cypress/e2e/environment-variable.cy.js
+++ cypress/e2e/environment-variable.cy.js
@@ -1,6 +1,14 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.then(() => {
+      console.log('###############################')
+      console.log('SECRET: ' + Cypress.env('MESSAGE'))
+      console.log('###############################')
+      
+      cy.log('cy ###############################')
+      cy.log('cy SECRET: ' + Cypress.env('MESSAGE'))
+      cy.log('cy ###############################')
+
       expect(Cypress.env('MESSAGE')).be.eq('CI GET ENVIRONMENT WITH SUCCESS')
     })
   })
