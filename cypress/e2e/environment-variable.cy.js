f6c3d2b21d13defee3edee880a7cf1799386177c
Nicolas Ferreira <81117784+nicolas-tf@users.noreply.github.com>
2022-10-05 07:13:58 -0300

    Remove import cypress.env

--- cypress/e2e/environment-variable.cy.js
+++ cypress/e2e/environment-variable.cy.js
@@ -1,9 +1,7 @@
-import * as file from '../../cypress.env.json'
 
 describe('empty spec', () => {
   it('passes', () => {
     cy.then(() => {
-      // expect(Cypress.env('MESSAGE')).be.eq('CI DATA')
       expect(Cypress.env('MESSAGE')).be.eq('CI DATA')
     })
   })
